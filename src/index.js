0b9765795927fd159350ceaf05bccf36290d29ed
Joyce_Pai <alicepai2020@gmail.com>
2023-06-02 20:09:30 +0800

    RWD

--- src/index.js
+++ src/index.js
@@ -1,31 +1,24 @@
-$(document).ready(function () {
-  $(".selfie-wrapper").animate(
-    {
-      opacity: 1,
-      left: "0",
-    },
-    1800,
-    "linear"
-  );
-  $(".content").animate(
-    {
-      opacity: 1,
-      right: "0",
-    },
-    1800,
-    "linear"
-  );
-  $(window).on("scroll resize", checkHeight);
-  function checkHeight() {
-    var windowHeight = $(window).height();
-    var clientScrollY = $(window).scrollTop();
-    var AboutTop = $("#about").offset().top;
-    var ResumeTop = $("#resume").offset().top;
-    var PorfolioTop = $("#porfolio").offset().top;
-    // if (clientScrollY >= AboutTop - 20) {
-    //   alert(AboutTop);
-    // var
-    //   $eachElement.
-    // }
+const menu = document.querySelector(".menu-icon");
+const navLinks = document.querySelector(".nav-right");
+const header = document.querySelector(".header");
+const closeIcon = document.querySelector(".fa-x");
+const openIcon = document.querySelector(".fa-bars");
+let isOpen = navLinks.style.display === "flex";
+
+menu.addEventListener("click", () => {
+  if (!isOpen) {
+    navLinks.style.display = "flex";
+    navLinks.style.height = "unset";
+    closeIcon.style.display = "flex";
+    openIcon.style.display = "none";
+    isOpen = !isOpen;
+  } else {
+    navLinks.style.display = "none";
+    header.style.alignItems = "center";
+    closeIcon.style.display = "none";
+    openIcon.style.display = "flex";
+    isOpen = !isOpen;
   }
 });
+
+// window.addEventListener("");
